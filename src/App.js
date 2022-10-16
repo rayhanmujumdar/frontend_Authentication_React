8612ef0a8efdbdd468977c57effa676a6c44acbd
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-10-16 11:57:14 -0500

    login section added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from './pages/Home'
 import { Toaster } from 'react-hot-toast';
 import { Route, Routes } from 'react-router-dom';
 import About from './pages/About';
+import Login from './Components/LogIn/Login'
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/signup" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/about" element={<About></About>}></Route>
       </Routes>
       <Toaster></Toaster>
--- src/Components/LogIn/Login.js
+++ src/Components/LogIn/Login.js
@@ -1,8 +1,48 @@
+import axios from "axios";
 import React from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async ({ password, email }) => {
+    const userAuth = {
+      email,
+      password,
+    };
+    console.log(userAuth);
+    try {
+      const { data } = await axios.get(
+        "http://localhost:4000/api/v1/auth/signIn",
+        {
+          params: userAuth,
+        }
+      );
+      console.log(data);
+      if (data.success) {
+        reset();
+        toast.success("SuccessFully signIn", { id: "success" });
+        navigate("/about");
+      } else {
+        toast.error(data.message, { id: "success" });
+      }
+    } catch (err) {
+      const { email, age, married } = err.response.data.errors;
+      toast.error(email.message || age.message || married.message, {
+        id: "errors",
+      });
+    }
+  };
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <section className="h-full gradient-form bg-gray-200 md:h-screen">
         <div className="container py-12 px-6 h-full">
           <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
@@ -21,37 +61,64 @@ const Login = () => {
                           We are The Lotus Team
                         </h4>
                       </div>
-                      <form>
+                      <form onSubmit={handleSubmit(onSubmit)}>
                         <p className="mb-4">Please login to your account</p>
                         <div className="mb-4">
                           <input
-                            type="text"
+                            {...register("email", {
+                              required: {
+                                value: true,
+                                message: "email must be required",
+                              },
+                              pattern: {
+                                value:
+                                  /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-z]+)$/,
+                                message: "Please provide a valid email",
+                              },
+                            })}
+                            type="email"
                             className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             id="exampleFormControlInput1"
-                            placeholder="Username"
+                            placeholder="email"
                           />
+                          {errors.email && (
+                            <p className="text-start text-red-500 mt-1">
+                              {errors.email.message}
+                            </p>
+                          )}
                         </div>
                         <div className="mb-4">
                           <input
+                            {...register("password", {
+                              required: {
+                                value: true,
+                                message: "password must be required",
+                              },
+                            })}
                             type="password"
                             className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             id="exampleFormControlInput1"
                             placeholder="Password"
                           />
+                          {errors.password && (
+                            <p className="text-start text-red-500 mt-1">
+                              {errors.password.message}
+                            </p>
+                          )}
                         </div>
                         <div className="text-center pt-1 mb-12 pb-1">
                           <button
                             className="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
-                            type="button"
+                            type="submit"
                             data-mdb-ripple="true"
                             data-mdb-ripple-color="light"
-                            style={{background: 'linear-gradient(to right,#ee7724,#d8363a,#dd3675,#b44593)'}}
+                            style={{
+                              background:
+                                "linear-gradient(to right,#ee7724,#d8363a,#dd3675,#b44593)",
+                            }}
                           >
                             Log in
                           </button>
-                          <a className="text-gray-500" href="#!">
-                            Forgot password?
-                          </a>
                         </div>
                         <div className="flex items-center justify-between pb-6">
                           <p className="mb-0 mr-2">Don't have an account?</p>
@@ -69,7 +136,10 @@ const Login = () => {
                   </div>
                   <div
                     className="lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none"
-                    style={{background: 'linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)'}}
+                    style={{
+                      background:
+                        "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                    }}
                   >
                     <div className="text-white px-4 py-6 md:p-12 md:mx-6">
                       <h4 className="text-xl font-semibold mb-6">
